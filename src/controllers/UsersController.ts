e9749e38156a102e77a6900fb3c3e9a922140d86
LuisMeirelles <luis.gustavo.souto.meirelles@gmail.com>
2021-01-05 03:55:14 -0300

    User CRUD finished

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -31,8 +31,93 @@ export default class UsersController {
             await trx.rollback();
 
             return response.status(400).json({
-                error: 'Unexpected error while creating new user'
+                message: 'Unexpected error while creating new user',
+                error
             });
         }
+    }
+
+    async index(request: Request, response: Response): Promise<Response> {
+        const filters = request.query;
+
+        try {
+            const users = await db('users')
+                .where({ ...filters });
+
+            return response.status(200).json(users);
+        } catch (error) {
+            return response.status(400).json({
+                message: 'Unexpected error while list users',
+                error
+            });
+        }
+    }
+
+    async show(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        try {
+            const user = await db('users')
+                .where({ id });
+
+            return response.status(200).json(user);
+        } catch (error) {
+            return response.status(400).json({
+                message: 'Unexpected error while show the user',
+                error
+            });
+        }
+    }
+
+    async update(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        const {
+            email,
+            username,
+            name,
+            password
+        } = request.body;
+
+        const trx = await db.transaction();
+
+        try {
+            await trx('users')
+                .update({
+                    email,
+                    username,
+                    name,
+                    password
+                })
+                .where({ id });
+
+            trx.commit();
+
+            return response.status(200).send();
+        } catch (error) {
+            await trx.rollback();
+
+            return response.status(400).json({
+                message: 'Unexpected error while update new user',
+                error
+            });
+        }
+    }
+
+    async delete(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        try {
+            await db('users')
+                .delete()
+                .where({ id });
+        } catch (error) {
+            return response.status(400).json({
+                message: 'Unexpected error while delete the user',
+                error
+            });
+        }
+
+        return response.status(200).send();
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -7,5 +7,9 @@ const routes = express.Router();
 const usersController = new UsersController();
 
 routes.post('/users', usersController.create);
+routes.get('/users', usersController.index);
+routes.get('/users/:id', usersController.show);
+routes.put('/users/:id', usersController.update);
+routes.delete('/users/:id', usersController.delete);
 
 export default routes;
