eceb15886cfc558918b013130c5c23df8d750070
Luis Meirelles <36140883+LuisMeirelles@users.noreply.github.com>
2021-01-11 03:58:57 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,8 +3,4 @@ import knexfile from '../../knexfile';
 
 const environment = process.env.ENVIRONMENT || 'development';
 
-if (environment === 'production') {
-    export default knex(knexfile.production);
-} else {
-    export default knex(knexfile.development);
-}
+export default knex(environment === 'development' ? knexfile.development : knexfile.production);
