4a552b934234d967c0bb21f5a05f1283db7f2c5a
Luis Meirelles <36140883+LuisMeirelles@users.noreply.github.com>
2021-01-11 03:48:53 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,4 +2,4 @@ import knex from 'knex';
 import knexfile from '../../knexfile';
 
 const environment = process.env.ENVIRONMENT || 'development';
-export default knex(eval(`knexfile.${environment}`));
+export default knex(knexfile[environment]);
